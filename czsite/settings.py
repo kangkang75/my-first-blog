5041b869701816b69cbfd9a393448889d025e826
kangkang75 <158245815@qq.com>
2016-12-29 10:50:35 +0800

    change ALLOWED HOSTS

--- czsite/settings.py
+++ czsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yz6d=8_t_4_q+az%h(0($kh6zun23-#v_cy+45v8r%xz81ix^g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [kangkang75.pythonanywhere.com]
 
 
 # Application definition
